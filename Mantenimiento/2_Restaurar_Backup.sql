041fee32e2b06c1dd2af50b2e1b90c359cafe37a
André Rodríguez Angeles <132963548+AndreRodHz@users.noreply.github.com>
2023-07-27 03:16:32 -0500

    Renovacion de archivos

--- Mantenimiento/2_Restaurar_Backup.sql
+++
@@ -1,9 +0,0 @@
-Use master
-
-----LAPTOP
---restore database SastreriaV1
---	from disk = 'D:\Backup Andre\SastreriaV1.bak'
-
-----PC MASTER
---restore database SastreriaV1
---	from disk = 'D:\Andre2\Backup\SastreriaV1.bak'
